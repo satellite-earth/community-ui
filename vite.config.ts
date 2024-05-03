0e39c847d36e94d9407fd183f213f600a9c6af86
hzrd149 <github@hzrd149.com>
2024-05-03 08:20:34 -0500

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// @ts-expect-error
-	base: process.env.VITE_BASE ?? '/',
+	base: process.env.VITE_BASE ?? './',
 	build: {
 		target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
 		sourcemap: true,
