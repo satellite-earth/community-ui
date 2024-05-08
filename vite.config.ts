4667ee01f0b507a36a6ed52f6ef0690beda9283c
hzrd149 <github@hzrd149.com>
2024-05-08 12:43:17 -0500

    add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,50 @@ export default defineConfig({
 		target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
 		sourcemap: true,
 	},
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			workbox: {
+				// This increase the cache limit to 3mB
+				maximumFileSizeToCacheInBytes: 2097152 * 1.5,
+			},
+			manifest: {
+				name: 'Satellite Communities',
+				short_name: 'Satellite',
+				description: 'An interface for satellite communities',
+				display: 'standalone',
+				orientation: 'portrait-primary',
+				theme_color: '#171819',
+				categories: ['social'],
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+				lang: 'en',
+				start_url: '/',
+				scope: '/',
+			},
+		}),
+	],
 });
